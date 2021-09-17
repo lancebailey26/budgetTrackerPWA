f6c649df36d77322c59211f4c742a03428e795a2
Lance Bailey <lancebailey222@gmail.com>
2021-09-17 17:55:42 -0400

    heroku deploy database link

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ app.use(express.static("src"));
 
 mongoose.connect(process.env.MONGDB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 // routes
