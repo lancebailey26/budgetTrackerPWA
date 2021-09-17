00318b8e76421f26f1238b2c0aadc289c255ff9e
Lance Bailey <lancebailey222@gmail.com>
2021-09-17 16:34:58 -0400

    heroku deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 let transactions = [];
 let myChart;
-import {saveRecord} from './db'
+// import {saveRecord} from './db'
 
 fetch("/api/transaction")
   .then(response => {
@@ -14,7 +14,17 @@ fetch("/api/transaction")
     populateTable();
     populateChart();
   });
-
+  const saveRecord = (record) => {
+    console.log('Save record invoked');
+    // Create a transaction on the BudgetStore db with readwrite access
+    const transaction = db.transaction(['BudgetStore'], 'readwrite');
+  
+    // Access your BudgetStore object store
+    const store = transaction.objectStore('BudgetStore');
+  
+    // Add record to your store with add method.
+    store.add(record);
+  };
 function populateTotal() {
   // reduce transaction amounts to a single total value
   let total = transactions.reduce((total, t) => {
